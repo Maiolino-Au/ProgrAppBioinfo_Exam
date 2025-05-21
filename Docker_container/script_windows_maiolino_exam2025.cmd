67785e38d3b9bffd19c2afbd5468b92c2a0e4437
Aurelio Maiolino <aurelio.maiolino@gmail.com>
2025-05-21 11:50:54 +0200

    Update script_windows_maiolino_exam2025.cmd

--- Docker_container/script_windows_maiolino_exam2025.cmd
+++ Docker_container/script_windows_maiolino_exam2025.cmd
@@ -3,6 +3,5 @@
 @If Not Exist "configurationFile_maiolino_exam2025.txt" Set /P "=%Build%" 0<NUL 1>"configurationFile_maiolino_exam2025.txt"
 mkdir %Build%
 copy configurationFile_maiolino_exam2025.txt %Build%
-del %Build%\id.txt
-docker run --platform linux/amd64 -itv %Build%:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --privileged=true --cidfile  %Build%\id.txt  -p  8888:8888 maiolino_exam2025
-
+del %Build%\id_maiolino_exam2025.txt
+docker run --platform linux/amd64 -itv %Build%:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --privileged=true --cidfile  %Build%\id_maiolino_exam2025.txt  -p  8888:8888 maiolino_exam2025
