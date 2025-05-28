88507d63040dde944a232f2a4b7b07d1880b95d3
Aurelio Maiolino <aurelio.maiolino@gmail.com>
2025-05-28 11:20:53 +0200

    Update script_windows_maiolino_exam2025.cmd

--- Docker_container/script_windows_maiolino_exam2025.cmd
+++ Docker_container/script_windows_maiolino_exam2025.cmd
@@ -1,7 +1,6 @@
-@Set "Build=%CD%"
-@Echo(%Build%
-@If Not Exist "configurationFile_maiolino_exam2025.txt" Set /P "=%Build%" 0<NUL 1>"configurationFile_maiolino_exam2025.txt"
-mkdir %Build%
-copy configurationFile_maiolino_exam2025.txt %Build%
-del %Build%\id_maiolino_exam2025.txt
-docker run --platform linux/amd64 -itv %Build%:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --privileged=true --cidfile  %Build%\id_maiolino_exam2025.txt  -p  8888:8888 maiolino_exam2025
+@echo off
+REM Ottieni il percorso corrente
+set "CURRENT_DIR=%cd%"
+
+REM Avvia Docker e monta la cartella come /sharedFolder
+docker run -it --rm -p 8888:8888 -v "%CURRENT_DIR%:/sharedFolder" maiolino_exam2025
