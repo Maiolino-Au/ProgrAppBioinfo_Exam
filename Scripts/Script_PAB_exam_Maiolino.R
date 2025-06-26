92ccd99358c944f717c14aaaaa0b25f90f174f28
Aurelio Maiolino <aurelio.maiolino@gmail.com>
2025-06-26 22:58:04 +0200

    Put name of library and comments

--- Scripts/Script_PAB_exam_Maiolino.R
+++ Scripts/Script_PAB_exam_Maiolino.R
@@ -1,47 +1,42 @@
 # Load libraries
-library(Maiolino_PAB_exam)
+library(PABMaiolinoPackage)
 
 # Step 1: Load sparse matrix and convert to full matrix
 dt <- s1.make.dt(data_path = "/sharedFolder/Data/matrix/")
-
 
 # Step 2: Split gene expression and ATAC-seq data
 dt_genes <- s2.subset.dt(string = "ENSG", data = dt)
 dt_atac <- s2.subset.dt(string = "chr", data = dt)
 
-
-# Step 3: Summarize Data___
+# Step 3: Summarize Data
 genes_summary <- s3.summarize.data(data = dt_genes)
 atac_summary <- s3.summarize.data(data = dt_atac)
-
 
 # Step 4: Create GenomicRanges
 gr_genes <- s4.create.GenomicRanges("genes")
 gr_atac <- s4.create.GenomicRanges("atac")
 
-
-# Step 5 ___________________________________________________________________________
+# Step 5: Gene Annotation for ATACseq data
+# Step 5 - Create GFT
 h38_coding <- s5.GR.protein.coding()
-
+# Step 5: Remap ATAC
 gr_atac <- s5.remap.atac()
 
-
-# Step 6 ___________________________________________________________________________
+# Step 6 Finalize expression data
 gr_genes <- s6.finalize.exp.data()
 
-
-# Step 7 ___________________________________________________________________________
-# Step 7 - Normalize _______________________________________________________________
+# Step 7 Data Normalization and Integration
+# Step 7 - Normalize 
 genes_cpm <- s7.normalize.cmp(data = genes_summary)
 atac_cpm <- s7.normalize.cmp(data = atac_summary)
 
 gr_genes$gene_cpm <- genes_cpm[match(gr_genes$gene_id, names(genes_cpm))]
 gr_atac$peak_cpm  <- atac_cpm[match(gr_atac$peak_id, names(atac_cpm))]
 
-# Step 7 - Merge ___________________________________________________________________
+# Step 7 - Merge 
 merged_data <- s7.merge()
 
-# Step 7 - Summary _________________________________________________________________
+# Step 7 - Summary 
 unmerged_peaks <- s7.summary.table.peaks()
 
 genes_no_atac <- s7.summary.table.genes()
@@ -52,6 +47,5 @@ plot_s7_genes <- s7.summary.plot.genes()
 
 chr_wise_summary <- s7.summary.chr.wise()
 
-
-# Step 8 ___________________________________________________________________________
+# Step 8: Visualization
 s8.save.plots()
