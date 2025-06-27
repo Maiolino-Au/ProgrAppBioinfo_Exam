c57c65104edf2bd3044cdade74649efdd73137ff
Aurelio Maiolino <aurelio.maiolino@gmail.com>
2025-06-27 03:19:22 +0200

    Update script_unix_maiolino_exam2025.sh

--- Docker_container/script_unix_maiolino_exam2025.sh
+++ Docker_container/script_unix_maiolino_exam2025.sh
@@ -7,4 +7,4 @@ tt=$(head configurationFile_maiolino_exam2025.txt)
 mkdir $tt
 cp ./configurationFile_maiolino_exam2025.txt $tt
 rm $tt/id_maiolino_exam2025.txt
-docker run --platform linux/amd64 -itv $tt:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --cidfile  $tt/id_maiolino_exam2025.txt --privileged=true -p  8888:8888 maiolino_exam2025
+docker run --platform linux/amd64 -itv $tt:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --cidfile  $tt/id_maiolino_exam2025.txt --privileged=true -p  8888:8888 maiolinoaurelio/pab_exam2025
