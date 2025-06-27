ce51b8193bb7300a4002c166aa1dffbb1ec86472
Aurelio Maiolino <aurelio.maiolino@gmail.com>
2025-06-27 03:19:49 +0200

    Update script_unix_maiolino_exam2025.sh

--- Docker_container/script_unix_maiolino_exam2025.sh
+++ Docker_container/script_unix_maiolino_exam2025.sh
@@ -1,10 +1,1 @@
-if test -f "./configurationFile_maiolino_exam2025.txt"; then
-echo "$FILE exists."
-else
-pwd > configurationFile_maiolino_exam2025.txt
-fi
-tt=$(head configurationFile_maiolino_exam2025.txt)
-mkdir $tt
-cp ./configurationFile_maiolino_exam2025.txt $tt
-rm $tt/id_maiolino_exam2025.txt
-docker run --platform linux/amd64 -itv $tt:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --cidfile  $tt/id_maiolino_exam2025.txt --privileged=true -p  8888:8888 maiolinoaurelio/pab_exam2025
+docker run -it —rm -p 8888:8888 -v "$(pwd)":/sharedFolder maiolinoaurelio/pab_exam2025
